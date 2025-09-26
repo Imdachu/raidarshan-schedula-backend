baa40a0d578a489f2fbf27dc91555eb74ad2746b
Imdachu <darshanraidarshan@gmail.com>
2025-09-26 18:00:11 +0530

    add

--- src/seeder/seeder.ts
+++ src/seeder/seeder.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { SeederModule } from './seeder.module';
 import { SeederService } from './seeder.service';
 
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+
 async function bootstrap() {
   const appContext = await NestFactory.createApplicationContext(SeederModule);
 
